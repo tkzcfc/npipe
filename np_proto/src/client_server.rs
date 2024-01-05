28c0e2cc9640f0076e18114a764ba6c3fd69595e
fang cheng <www.tkzc@foxmail.com>
2024-01-05 17:33:59 +0800

    update

--- np_proto/src/client_server.rs
+++ np_proto/src/client_server.rs
@@ -22,3 +22,15 @@ pub struct RegisterReq {
     #[prost(string, tag = "2")]
     pub password: ::prost::alloc::string::String,
 }
+/// 管理员登录
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct ManagementLoginReq {
+    /// @build_automatically_generate_message_id@  enum MsgId {None = 0; Id = 1005;}
+    /// 用户名
+    #[prost(string, tag = "1")]
+    pub username: ::prost::alloc::string::String,
+    /// 密码
+    #[prost(string, tag = "2")]
+    pub password: ::prost::alloc::string::String,
+}
