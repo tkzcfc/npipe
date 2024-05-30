15d0441e9f4892e4a6d52779a6a45ea2169916cd
fang cheng <www.tkzc@foxmail.com>
2024-05-30 18:54:52 +0800

    添加tcp udp入口

--- np_base/src/proxy/inlet.rs
+++ np_base/src/proxy/inlet.rs
@@ -1,51 +1,113 @@
-use std::io;
+use crate::net::session::WriterMessage;
+use crate::net::session_logic::SessionLogic;
+use crate::net::tcp_server;
+use async_trait::async_trait;
+use bytes::BytesMut;
 use std::net::SocketAddr;
-use log::info;
-use tokio::net::TcpListener;
+use std::sync::Arc;
 use tokio::net::UdpSocket;
+use tokio::net::{TcpListener, TcpStream};
+use tokio::select;
+use tokio::sync::mpsc::{Sender, UnboundedSender};
+use tokio::sync::{mpsc, Notify};
 
-enum InletProxyType {
+pub enum InletProxyType {
     TCP,
     UDP,
 }
 
-struct Inlet {
+pub struct Inlet {
     proxy: InletProxyType,
+    shutdown_tx: Option<Sender<()>>,
+    notify: Arc<Notify>,
 }
 
 impl Inlet {
     pub fn new(proxy: InletProxyType) -> Self {
         Self {
-            proxy
+            proxy,
+            shutdown_tx: None,
+            notify: Arc::new(Notify::new()),
         }
     }
 
-    pub async fn start(&self, listen_addr: String) -> anyhow::Result<()> {
+    pub async fn start(&mut self, listen_addr: String) -> anyhow::Result<()> {
+        self.stop().await;
+
+        let (shutdown_tx, mut shutdown_rx) = mpsc::channel(1);
+
+        self.shutdown_tx = Option::Some(shutdown_tx);
+
         let addr = listen_addr.parse::<SocketAddr>()?;
+        let worker_notify = self.notify.clone();
+
         match self.proxy {
             InletProxyType::TCP => {
                 let listener = TcpListener::bind(addr).await?;
-            },
+                tokio::spawn(async move {
+                    tcp_server::run_server(
+                        listener,
+                        || Box::new(InletSession::new()),
+                        |stream: TcpStream| async move { Ok(stream) },
+                        async move {
+                            let _ = shutdown_rx.recv().await;
+                        },
+                    )
+                    .await;
+                    worker_notify.notify_one();
+                });
+            }
             InletProxyType::UDP => {
                 let udpsocket = UdpSocket::bind(addr).await?;
+                tokio::spawn(async move {
+                    // 循环读取中...
+                    let mut buf = [0; 1024];
+                    let recv_task = async { loop {
+                        // 接收数据
+                        if let Ok((len, addr)) = udpsocket.recv_from(&mut buf).await {
+                            println!("Received {} bytes from {}", len, addr);
+                        }
+                    }};
 
+                    select! {
+                        _= recv_task => {},
+                        _= shutdown_rx.recv() => {}
+                    };
+                    worker_notify.notify_one();
+                });
             }
         }
 
-
-        // match addr {
-        //     Ok(addr) => {
-        //         info!("Start listening: {}", addr);
-        //         TcpListener::bind(addr).await
-        //     }
-        //     Err(parse_error) => Err(std::io::Error::new(
-        //         io::ErrorKind::InvalidInput,
-        //         parse_error.to_string(),
-        //     )),
-        // }
-
         Ok(())
+    }
+
+    pub async fn stop(&mut self) {
+        if self.shutdown_tx.is_some() {
+            self.shutdown_tx.take();
+            self.notify.notified().await;
+        }
     }
 }
 
+struct InletSession {}
 
+impl InletSession {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+#[async_trait]
+impl SessionLogic for InletSession {
+    fn on_session_start(&mut self, _session_id: u32, _tx: UnboundedSender<WriterMessage>) {}
+
+    async fn on_session_close(&mut self) {}
+
+    fn on_try_extract_frame(&self, buffer: &mut BytesMut) -> anyhow::Result<Option<Vec<u8>>> {
+        Ok(Some(buffer.to_vec()))
+    }
+
+    async fn on_recv_frame(&mut self, _frame: Vec<u8>) -> bool {
+        true
+    }
+}
--- np_base/src/proxy/mod.rs
+++ np_base/src/proxy/mod.rs
@@ -1,1 +1,24 @@
 mod inlet;
+
+#[cfg(test)]
+mod tests {
+    use crate::proxy::inlet::{Inlet, InletProxyType};
+    use std::time::Duration;
+    use tokio::time::sleep;
+
+    #[tokio::test]
+    async fn tes_inlet_stop() {
+        let mut inlet = Inlet::new(InletProxyType::TCP);
+        inlet.start("0.0.0.0:4000".into()).await.unwrap();
+
+        sleep(Duration::from_secs(1)).await;
+
+        inlet.start("0.0.0.0:4000".into()).await.unwrap();
+        sleep(Duration::from_secs(1)).await;
+
+        inlet.stop().await;
+        sleep(Duration::from_secs(1)).await;
+        inlet.stop().await;
+        inlet.stop().await;
+    }
+}
