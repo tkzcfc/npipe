edaced9018f291e9ea0f80d1001d0258e1f7f863
fang cheng <www.tkzc@foxmail.com>
2024-06-21 18:58:10 +0800

    outlet

--- np_base/src/proxy/outlet.rs
+++ np_base/src/proxy/outlet.rs
@@ -1,16 +1,20 @@
 use crate::proxy::{OutputFuncType, ProxyMessage};
+use bytes::BytesMut;
+use log::{debug, info};
 use std::collections::HashMap;
 use std::net::SocketAddr;
-use tokio::io::{AsyncWriteExt, ReadHalf, WriteHalf};
+use tokio::io::{AsyncReadExt, AsyncWriteExt, WriteHalf};
 use tokio::net::{TcpSocket, TcpStream, UdpSocket};
 use tokio::sync::Mutex;
 
+type TcpWriter = Mutex<WriteHalf<TcpStream>>;
+
 enum ClientType {
-    TCP(TcpClient),
+    TCP(TcpWriter),
     UDP(SocketAddr),
 }
 
-struct Outlet {
+pub struct Outlet {
     client_map: Mutex<HashMap<u32, ClientType>>,
     udp_socket: Mutex<Option<UdpSocket>>,
     on_output_callback: OutputFuncType,
@@ -28,52 +32,53 @@ impl Outlet {
     pub async fn input(&self, message: ProxyMessage) -> anyhow::Result<()> {
         match message {
             ProxyMessage::I2oConnect(session_id, is_tcp, addr) => {
+                info!("session: {session_id}, connect to: {addr}");
                 if self.client_map.lock().await.contains_key(&session_id) {
-                    let _ = (self.on_output_callback)(ProxyMessage::O2iConnect(
+                    (self.on_output_callback)(ProxyMessage::O2iConnect(
                         session_id,
                         false,
-                        "Repeated connection".into(),
+                        "repeated connection".into(),
                     ))
                     .await?;
-                    return Ok(());
-                }
-                if is_tcp {
-                    match TcpClient::connect(addr).await {
-                        Ok(client) => {
-                            self.client_map
-                                .lock()
-                                .await
-                                .insert(session_id, ClientType::TCP(client));
-                        }
-                        Err(err) => {
-                            // 连接失败
-                            let _ = (self.on_output_callback)(ProxyMessage::O2iConnect(
-                                session_id,
-                                false,
-                                err.to_string(),
-                            ))
-                            .await?;
-                            return Ok(());
-                        }
-                    }
                 } else {
-                    self.client_map
-                        .lock()
-                        .await
-                        .insert(session_id, ClientType::UDP(addr));
+                    if is_tcp {
+                        match tcp_connect(addr, session_id, self.on_output_callback.clone()).await {
+                            Ok(client) => {
+                                self.client_map
+                                    .lock()
+                                    .await
+                                    .insert(session_id, ClientType::TCP(client));
+                            }
+                            Err(err) => {
+                                // 连接失败
+                                let _ = (self.on_output_callback)(ProxyMessage::O2iConnect(
+                                    session_id,
+                                    false,
+                                    err.to_string(),
+                                ))
+                                .await?;
+                                return Ok(());
+                            }
+                        }
+                    } else {
+                        self.client_map
+                            .lock()
+                            .await
+                            .insert(session_id, ClientType::UDP(addr));
+                    }
+                    (self.on_output_callback)(ProxyMessage::O2iConnect(
+                        session_id,
+                        true,
+                        "".into(),
+                    ))
+                    .await?;
                 }
-                let _ = (self.on_output_callback)(ProxyMessage::O2iConnect(
-                    session_id,
-                    true,
-                    "".into(),
-                ))
-                .await?;
             }
             ProxyMessage::I2oSendData(session_id, data) => {
                 if let Some(client_type) = self.client_map.lock().await.get(&session_id) {
                     match client_type {
-                        ClientType::TCP(client) => {
-                            client.send(data).await?;
+                        ClientType::TCP(writer) => {
+                            writer.lock().await.write_all(&data).await?;
                         }
                         ClientType::UDP(addr) => {
                             let mut socket = self.udp_socket.lock().await;
@@ -87,7 +92,10 @@ impl Outlet {
                     }
                 }
             }
-            ProxyMessage::I2oDisconnect(session_id) => {}
+            ProxyMessage::I2oDisconnect(session_id) => {
+                info!("disconnect session: {session_id}");
+                self.client_map.lock().await.remove(&session_id);
+            }
             _ => {
                 panic!("error message")
             }
@@ -96,52 +104,33 @@ impl Outlet {
     }
 }
 
-struct TcpClient {
-    reader: ReadHalf<TcpStream>,
-    writer: Mutex<WriteHalf<TcpStream>>,
+async fn tcp_connect(
+    addr: SocketAddr,
+    session_id: u32,
+    on_output_callback: OutputFuncType,
+) -> anyhow::Result<TcpWriter> {
+    let socket = if addr.is_ipv4() {
+        TcpSocket::new_v4()?
+    } else {
+        TcpSocket::new_v6()?
+    };
+    let stream = socket.connect(addr).await?;
+    let (mut reader, writer) = tokio::io::split(stream);
+
+    tokio::spawn(async move {
+        let mut buffer = BytesMut::with_capacity(1024);
+        loop {
+            match reader.read_buf(&mut buffer).await {
+                // n为0表示对端已经关闭连接。
+                Ok(n) if n == 0 => {
+                    debug!("socket[{}] closed.", addr);
+                    break;
+                }
+                _ => {}
+            }
+        }
+        let _ = on_output_callback(ProxyMessage::O2iDisconnect(session_id)).await;
+    });
+
+    Ok(Mutex::new(writer))
 }
-
-impl TcpClient {
-    async fn connect(addr: SocketAddr) -> anyhow::Result<Self> {
-        let socket = if addr.is_ipv4() {
-            TcpSocket::new_v4()?
-        } else {
-            TcpSocket::new_v6()?
-        };
-        let stream = socket.connect(addr).await?;
-        let (reader, writer) = tokio::io::split(stream);
-        Ok(Self {
-            reader,
-            writer: Mutex::new(writer),
-        })
-    }
-
-    async fn send(&self, frame: Vec<u8>) -> anyhow::Result<()> {
-        self.writer.lock().await.write_all(&frame).await?;
-        Ok(())
-    }
-}
-
-//
-// struct UdpClient {
-//     socket: UdpSocket,
-// }
-//
-//
-// impl UdpClient {
-//     async fn connect(addr: SocketAddr) -> anyhow::Result<Self> {
-//         let socket = UdpSocket::bind("0.0.0.0:0").await?;
-//         socket.connect(&addr).await?;
-//
-//         let socket_cloned = socket.clone();
-//
-//         Ok(Self {
-//             socket
-//         })
-//     }
-//
-//     async fn send(&mut self, frame: Vec<u8>) -> anyhow::Result<()> {
-//         self.socket.send(&frame).await?;
-//         Ok(())
-//     }
-// }
