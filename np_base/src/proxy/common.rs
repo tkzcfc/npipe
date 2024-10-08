7149090c568f3131b6d2f852783032229f2584c6
fang cheng <www.tkzc@foxmail.com>
2024-10-08 12:15:57 +0800

    去掉缓存大小限制

--- np_base/src/proxy/common.rs
+++ np_base/src/proxy/common.rs
@@ -66,15 +66,15 @@ impl SessionCommonInfo {
             )?;
         }
 
-        let read_buf_max: usize = if self.is_inlet {
-            READ_BUF_MAX_LEN
-        } else {
-            READ_BUF_MAX_LEN * 5
-        };
-
-        while *self.read_buf_len.read().await > read_buf_max {
-            yield_now().await;
-        }
+        // let read_buf_max: usize = if self.is_inlet {
+        //     READ_BUF_MAX_LEN
+        // } else {
+        //     READ_BUF_MAX_LEN * 5
+        // };
+        //
+        // while *self.read_buf_len.read().await > read_buf_max {
+        //     yield_now().await;
+        // }
 
         let mut read_buf_len_rw = self.read_buf_len.write().await;
         *read_buf_len_rw = *read_buf_len_rw + data.len();
