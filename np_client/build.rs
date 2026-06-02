295f9afebd77eef66fa3719ff02924f8ae4c76b0
tkzcfc <tkzcmemeda@gmail.com>
2026-06-02 14:52:22 +0800

    update

--- np_client/build.rs
+++ np_client/build.rs
@@ -1,4 +1,5 @@
 fn main() {
-    // 让 cargo 追踪 BIN_VERSION 环境变量，值变了就重编译
-    println!("cargo:rerun-if-env-changed=BIN_VERSION");
+    if let Ok(version) = std::env::var("BIN_VERSION") {
+        println!("cargo:rustc-env=BIN_VERSION={version}");
+    }
 }
